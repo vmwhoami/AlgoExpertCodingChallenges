cec3b7e12b49a62398b52fb2900e604d4351e6b4
vmwhoami <vmwhoami@gmail.com>
2021-04-16 11:41:29 +0300

    Solved closest iteratively and recursive

--- ruby/closestValBST.rb
+++ ruby/closestValBST.rb
@@ -47,12 +47,52 @@ class BST
    end 
    arr.min_by{|x| (value-x).abs} 
  end
- 
+# Recursive Averege time O(log(n)) | space O(log(n))  Worst O(n)| O(n)
+
+# Recursive
+def findClosestValHelper(tree,target,closest)
+ return closest if tree.nil?
+  if (target-closest).abs > (target-tree.value).abs
+    closest = tree.value
+  end
+  if target < tree.value
+   return findClosestValHelper(tree.left,target,closest)
+  elsif target > tree.value
+    return findClosestValHelper(tree.right,target,closest)
+  else
+    return closest
+  end
+end
+
+ def findClosestVal(target)
+  self.findClosestValHelper(tree =self,target,closest = self.value)
+ end
+# Iteratively
+
+
+  def findClosestIteratively(target)
+    closest = self.value
+    current = self
+    while !current.nil?
+      closest = current.value if (target-closest).abs > (target-current.value).abs
+      if target < current.value
+        current = current.left
+      elsif target > current.value
+        current = current.right
+      else
+        break
+      end
+    end
+    return closest
+  end
+
+
 
 end
 
 bst = BST.new(10)
-bst.createFromArray([5,5,2,1,15,13,14,22,33,21,50])
-closest = bst.findCLosest(12)
+bst.createFromArray([5,5,2,1,15,13,14, 22,33,21,50])
+# closest = bst.findCLosest(12)
+closest = bst.findClosestIteratively(11)
 p closest
 # => 13
